d2e962f60f8afa26140a25f9b5accb288e884ca8
ningchen8743 <ning.chen.cathy@gmail.com>
2018-09-22 19:54:51 -0400

    Preliminary folder set up

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+require 'helpers.php';
+require 'logic.php';
+?>
+
+<!DOCTYPE html>
+<html lang='en'>
+
+<head>
+    <title>Project 2 for Fall semester</title>
+    <meta charset='utf-8'>
+
+</head>
+<body>
+
+<h1>Project 2</h1>
+
+
+</body>
+</html>
